498c4717e707bb87f03f04790fc819e3ef6df3be
Piotrek <potrek40@wp.pl>
2021-07-22 12:27:28 +0200

    Thumbnail skeletons

--- components/ui/MediaRow/MediaRow.js
+++ components/ui/MediaRow/MediaRow.js
@@ -1,6 +1,8 @@
-
+import {useState} from 'react';
 
 function MediaRow({title, type}) {
+    const [loadingData, setLoadingData] = useState(true);
+
     const loopComp = (comp, times) => {
         let thumbnails = [];
         for(let i = 0; i < times; i++){
@@ -8,11 +10,19 @@ function MediaRow({title, type}) {
         }
         return thumbnails;
     }
+
+    const showThumbnails = () => {
+        setTimeout(() => setLoadingData(false), 3000);
+        return loopComp(loadingData ? <Skeleton/> : <Thumnail />, 10);
+    }
+
     return (
         <div className={`media-row ${type}`}>
             <h3 className="media-row__title">{title}</h3>
             <div className="media-row__thumbnails">
-                {loopComp(
+                {showThumbnails()}
+                {/* <Skeleton /> */}
+                {/* {loopComp(
                     <div className="media-row__thumbnail">
                         <img src="https://creativereview.imgix.net/content/uploads/2019/12/joker_full.jpg?auto=compress,format&q=60&w=1012&h=1500" alt="rickmorty" />
                         <div className="media-row__top-layer">
@@ -20,10 +30,32 @@ function MediaRow({title, type}) {
                         </div>
                     </div>, 10
                    
-                )}
+                )} */}
             </div>
         </div>
     )
 }
+
+function Thumnail() {
+    return (
+        <div className="media-row__thumbnail">
+            <img src="https://creativereview.imgix.net/content/uploads/2019/12/joker_full.jpg?auto=compress,format&q=60&w=1012&h=1500" alt="rickmorty" />
+            <div className="media-row__top-layer">
+                <i className="fas fa-play" />
+            </div>
+        </div>
+    )
+}
+
+function Skeleton() {
+    return (
+        <div className="media-row__thumbnail-skeleton">
+            <div className="media-row__thumbnail-skeleton-img">
+
+            </div>
+        </div>
+    )
+}
+
 
 export default MediaRow
